5b514f64542404e631145b158e55c23b5d65cd70
Kenil Shah <kenilshah93@gmail.com>
2026-06-18 15:10:33 +0530

    feat: add math utility functions with initial add implementation

---
+++ src/utils/mathUtils.js
@@ -0,0 +1,13 @@
+/**
+ * Math utility functions.
+ */
+
+/**
+ * Adds two numbers.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+export const add = (a, b) => {
+  return a + b;
+};
