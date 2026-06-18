6a7b011cce5fd4be8055273e3e0d347ea2a0147e
Kenil Shah <kenilshah93@gmail.com>
2026-06-18 15:21:40 +0530

    feat: add calculateAverage utility function to mathUtils

--- src/utils/mathUtils.js
+++ src/utils/mathUtils.js
@@ -11,3 +11,16 @@
 export const add = (a, b) => {
   return a + b;
 };
+
+/**
+ * Calculates the average of an array of numbers.
+ * @param {number[]} numbers 
+ * @returns {number}
+ */
+export const calculateAverage = (numbers) => {
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return 0;
+  }
+  const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+  return sum / numbers.length;
+};
