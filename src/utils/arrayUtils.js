083289d023c2dfb31e5e8c28c15d7abcdf8d89da
Kenil Shah <kenilshah93@gmail.com>
2026-06-25 11:37:39 +0530

    feat: add groupBy utility function to arrayUtils

--- src/utils/arrayUtils.js
+++ src/utils/arrayUtils.js
@@ -25,3 +25,26 @@ export const flatten = (arr) => {
   }
   return arr.reduce((acc, val) => acc.concat(val), []);
 };
+
+/**
+ * Groups an array of objects by a given key.
+ * @param {Object[]} arr
+ * @param {string} key
+ * @returns {Object}
+ */
+export const groupBy = (arr, key) => {
+  if (!Array.isArray(arr)) {
+    throw new Error("Input must be an array");
+  }
+  if (typeof key !== "string" || !key) {
+    throw new Error("Key must be a non-empty string");
+  }
+  return arr.reduce((result, item) => {
+    const group = item[key];
+    if (!result[group]) {
+      result[group] = [];
+    }
+    result[group].push(item);
+    return result;
+  }, {});
+};
